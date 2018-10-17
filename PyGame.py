2e6fecc885e0965664477474f61da6570f89ea84
henrik392 <44232546+henrik392@users.noreply.github.com>
2018-10-17 14:41:44 +0200

    Add files via upload

---
+++ PyGame.py
@@ -0,0 +1,22 @@
+import pygame
+from pygame.locals import *
+
+pygame.init()
+
+screen = pygame.display.set_mode((800, 600))
+
+surf = pygame.Surface((50, 50))
+surf.fill((255, 255, 255))
+rect = surf.get_rect()
+
+gameOn = True
+
+while gameOn:
+    for event in pygame.event.get():
+        if event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                gameOn = False
+        elif event.type == QUIT:
+            gameOn = False
+    
+    
