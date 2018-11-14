329d7d2c3c3d025008a21f80613a197c8387f427
Henrik Halvorsen Kvamme <henkva29@askoyskolene.no>
2018-11-14 14:30:17 +0100

    bbbbbbbbb

--- PyGame.py
+++ PyGame.py
@@ -8,7 +8,7 @@ pygame.display.set_caption("Bee movie")
 screenWidth = 500
 
 x = 50
-y = 250
+y = 430
 width = 40
 height = 60
 vel = 5
@@ -31,13 +31,25 @@ while gameOn:
         x -= vel
     if keys[pygame.K_RIGHT] and x < screenWidth - width - vel:
         x += vel
-    if
-    if keys[pygame.K_UP] and y > vel:
-        y -= vel
-    if keys[pygame.K_DOWN] and y < 500 - height - vel:
-        y += vel
-    if keys[pygame.K_SPACE]:
-        isJumping = True
+    if not(isJumping):
+        if keys[pygame.K_UP] and y > vel:
+            y -= vel
+        if keys[pygame.K_DOWN] and y < 500 - height - vel:
+            y += vel
+        if keys[pygame.K_SPACE]:
+            isJumping = True
+    else:
+        if jumpCount >= -10:
+            neg = 1
+            if jumpCount < 0:
+                neg = -1
+            y -= (jumpCount ** 2) * 0.5 * neg
+            jumpCount -= 1
+        else:
+            isJumping = False
+            jumpCount = 10
+
+
 
     win.fill((0, 0, 0))
     pygame.draw.rect(win, (0, 200, 0), (x, y, width, height))
