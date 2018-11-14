86230bf4cc3cbdfcb9f17cb29caceadaa20f1461
Henrik Halvorsen Kvamme <henkva29@askoyskolene.no>
2018-11-14 12:48:21 +0100

    newnew

--- PyGame.py
+++ PyGame.py
@@ -1,19 +1,6 @@
 import pygame
+pygame.init()
 
 pygame.init()
-screen = pygame.display.set_mode((800, 600))
-
-is_blue = True
-
-
-done = False
-
-while not done:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done = True
-
-    pygame.draw.rect(screen, (0, 128, 255), pygame.Rect(60, 60, 120, 120))
-    
-    pygame.display.flip()
+window = pygame.display.set_mode((500, 500))
 
